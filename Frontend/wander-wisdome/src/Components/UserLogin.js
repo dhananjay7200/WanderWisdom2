ca9d95cd8fa093ad509fc5fe4bcf3667a6289704
Dhananjay Sudhakar Khuje <dhananjaykh@cybage.com>
2023-12-14 19:12:34 +0530

    User login html

--- Frontend/wander-wisdome/src/Components/UserLogin.js
+++ Frontend/wander-wisdome/src/Components/UserLogin.js
@@ -1,1 +1,53 @@
+import '../Styles/Login.css';
 
+
+export default function UserLogin(){
+
+
+    return (
+    <section class="vh-100">
+        <link></link>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-6 text-black">
+  
+          <div class="px-5 ms-xl-4">
+            <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" Style="color: #709085;"></i>
+            <span class="h1 fw-bold mb-0">Wander Wisdom</span>
+          </div>
+  
+          <div >
+            <form Style="width: 23rem;">
+  
+              <h3 class="fw-normal mb-3 pb-3" Style="letter-spacing: 1px;">Log in</h3>
+  
+              <div class="form-outline mb-4">
+                <input type="email" id="form2Example18" class="form-control form-control-lg" />
+                <label class="form-label" for="form2Example18">Email address</label>
+              </div>
+  
+              <div class="form-outline mb-4">
+                <input type="password" id="form2Example28" class="form-control form-control-lg" />
+                <label class="form-label" for="form2Example28">Password</label>
+              </div>
+  
+              <div class="pt-1 mb-4">
+                <button class="btn btn-info btn-lg btn-block" type="button">Login</button>
+              </div>
+  
+              <p class="small mb-5 pb-lg-2"><a class="text-muted" href="#!">Forgot password?</a></p>
+              <p>Don't have an account? <a href="#!" class="link-info">Register here</a></p>
+  
+            </form>
+  
+          </div>
+  
+        </div>
+        <div class="col-sm-6 px-0 d-none d-sm-block">
+         
+        </div>
+      </div>
+    </div>
+  </section>
+  );
+}
